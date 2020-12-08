1b8dd33efc2cab2e37e59acf9ed2dc0113a56d02
anis3139 <anis904692@gmail.com>
2020-12-08 09:00:09 +0600

    Class 9

---
+++ class-9/asstets/js/main.js
@@ -0,0 +1,26 @@
+// document.getElementById('test').style.color="red";
+// document.getElementById('test').style.backgroundColor="blue";
+
+// document.getElementsByClassName('testClass').style.color="green"
+
+// document.getElementsByTagName("h1").style.backgroundColor='red';
+
+// document.querySelectorAll('h1').innerHTML="Hi"
+
+// document.getElementById("btn").addEventListener('click',function () {
+//     document.getElementById("test").style.backgroundColor="red"
+//     document.getElementById("test").style.color="blue"
+// })
+
+
+
+    // $("#btn").click(function () {
+    //    $("#test").css('color', 'red')
+    //    $("#test").css('backgroundColor', 'blue')
+    // });
+
+$("#btn").click(function(){
+    $("#test").text("Hello World");
+   
+})
+
